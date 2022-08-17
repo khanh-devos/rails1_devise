f42f7674fad583afb736aafe4e9d7e7a102f9e53
khanh-devos <dreamproperty.khanh@gmail.com>
2022-08-17 14:58:32 +0700

    modifire create.html.erb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # end
 
   # Set Action Cable server url for consumer connection
-  # config.action_cable.url = 'ws://localhost:28080'
+  config.action_cable.url = 'ws://localhost:28080'
   # config.action_cable.worker_pool_size = 4
   
 end
